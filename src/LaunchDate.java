87d5138cd03df34a6ec8282696ac0a94b027be57
sadhana1808 <sadhana18082001@gmail.com>
2024-06-25 10:54:29 +0530

    Merge remote-tracking branch 'origin/main' # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- src/LaunchDate.java
+++ src/LaunchDate.java
@@ -1,16 +1,15 @@
-import java.util.*;
-import java .util.Date;
+import java.util.Scanner;
 
-//public class LaunchDate {
-//    public static void main(String[] args) {
-//        Scanner sc= new Scanner(System.in);
-//        String A = sc.next();
-//        String B = sc.next();
-//
-//        }
-//        System.out.println(A.length());
-//
-//    }
+public class LaunchDate {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String A = sc.next();
+        String B = sc.next();
+        System.out.println(A.length());
+        }
+
+
+    }
 
 
 
